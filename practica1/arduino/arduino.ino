360b3abcda1818d15f705a94e1c0f30044f9c933
orlando26 <orlandolozaarr@gmail.com>
2016-09-07 14:43:20 -0500

    dssada

--- practica1/arduino/arduino.ino
+++ practica1/arduino/arduino.ino
@@ -1,9 +1,23 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial bt(10, 11);
 void setup() {
-  // put your setup code here, to run once:
-
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  bt.begin(9600);
 }
 
+// the loop routine runs over and over again forever:
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // print out the value you read:
+  if(sensorValue >= 50){
+    Serial.println("1");
+    bt.println("1");
+    }else{
+      Serial.println("0");
+      bt.println("0");
 }
+  
+} 
